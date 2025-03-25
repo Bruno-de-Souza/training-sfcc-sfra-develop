3dd25bae1b330ec7250f28ce593e262273717f97
Bruno <brunomoreiramg@hotmail.com>
2025-03-24 21:55:35 -0300

    feature: add cart total threshold message and style

--- cartridges/app_training/cartridge/controllers/Cart.js
+++ cartridges/app_training/cartridge/controllers/Cart.js
@@ -1,19 +1,27 @@
 'use strict';
 
 var server = require('server');
-
 server.extend(module.superModule);
 
+var BasketMgr = require('dw/order/BasketMgr');
+var Site = require('dw/system/Site');
+
 server.append('Show', function (req, res, next) {
+    var currentBasket = BasketMgr.getCurrentBasket();
+    var cartTotal = currentBasket.totalGrossPrice.value;
+    var cartTotalThreshold = Site.getCurrent().getCustomPreferenceValue('cartTotalThreshold');
 
-    var viewData = res.getViewData();
+    if (cartTotal >= cartTotalThreshold) {
+        res.setViewData({
+            cartMessage: 'Your cart total exceeds $' + cartTotalThreshold + '!'
+        });
+    } else {
+        res.setViewData({
+            cartMessage: ''
+        });
+    }
 
-    viewData.example = 'Just a String';
-
-    res.setViewData(viewData);
-
-    return next();
-
+    next();
 });
 
 module.exports = server.exports();
